4ef8583940051ad3bee35420e004dc7e9c45cdec
Tobias Trelle <tobias.trelle@codecentric.de>
2012-02-06 08:44:10 +0100

    Box vs. Circle search comparison (Spring Data MongoDB)

--- springdatamongodb/src/test/java/mongodb/MongoDBMassTest.java
+++ springdatamongodb/src/test/java/mongodb/MongoDBMassTest.java
@@ -3,13 +3,16 @@ package mongodb;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.Box;
 import org.springframework.data.mongodb.core.geo.Circle;
+import org.springframework.data.mongodb.core.geo.Point;
 import org.springframework.data.mongodb.core.index.GeospatialIndex;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -26,7 +29,13 @@ public class MongoDBMassTest {
 
 	private static final int N_POINTS = 100000;
 
-	private static final int N_QUERIES = 100;
+	private static final int N_QUERIES = 10000;
+	
+	/** Radius for circle search. */
+	private static final double RADIUS = 0.1;
+	
+	/** Half the width of a box with the same area as a circle with r = {@link #RADIUS}. */
+	private static final double HALF_BOX_WIDTH = RADIUS * Math.sqrt(Math.PI) / 2;
 	
 	@Autowired
 	LocationRepository repo;
@@ -34,7 +43,8 @@ public class MongoDBMassTest {
 	@Autowired
 	MongoTemplate template;
 
-	@Before public void setUp() {
+	@Before 
+	public void setUp() {
 		final List<Location> locations = new ArrayList<Location>();
 		
 		 // ensure geospatial index
@@ -42,7 +52,7 @@ public class MongoDBMassTest {
 		 
 		 // prepare data
 		 for ( int i = 0;  i < N_POINTS; i++ ) {
-			 locations.add( new Location(String.valueOf(i), 2 * Math.random() - 1, 2 * Math.random() - 1) );
+			 locations.add( new Location(String.valueOf(i), rnd(), rnd()) );
 		 }
 		 out("Preparing data (" + N_POINTS + " locations) ...");
 		 repo.save(locations);
@@ -51,26 +61,51 @@ public class MongoDBMassTest {
 	
 	@Test
 	public void shouldPerformRandomQueries() {
-		long time = 0;
+		long time_circle = 0;
+		long time_box = 0;
 		long singleRun = 0;
 		Circle c;
+		Box b;
+		long n_circle = 0;
+		long n_box = 0;
+		List<Location> result;
 		
 		out( "Performing " + N_QUERIES + " queries ..." );
 		for (int i = 0; i < N_QUERIES; i++) {
-			// random search
-			c = new Circle(2 * Math.random() - 1, 2 * Math.random() - 1, 0.1);
 			
-			// perform query
+			double x = rnd(), y = rnd();
+			
+			// circle search
+			c = new Circle( x, y, RADIUS);
+			// box search
+			b = new Box( new Point(x - HALF_BOX_WIDTH, y - HALF_BOX_WIDTH), new Point(x + HALF_BOX_WIDTH, y + HALF_BOX_WIDTH) );
+			
+			// perform circle query
 			singleRun = System.currentTimeMillis();
-			repo.findByPositionWithin(c);
+			result = repo.findByPositionWithin(c);
 			singleRun = System.currentTimeMillis() - singleRun;
+			n_circle += result.size();
+			time_circle += singleRun;
 			
-			time += singleRun;
+			// peform box search
+			singleRun = System.currentTimeMillis();
+			result = repo.findByPositionWithin(b);
+			singleRun = System.currentTimeMillis() - singleRun;
+			n_box += result.size();
+			time_box += singleRun;
+			
 		}
 		out( "Done.\n" );
 		
-		out( "Overall time for " + N_QUERIES + " queries [ms]: " + time );
-		out( "Average time per query [ms]: " + (double)time / N_QUERIES );
+		out( "Search by circle:" );
+		out( "Overall time for " + N_QUERIES + " queries [ms]: " + time_circle );
+		out( "Average time per query [ms]: " + (double)time_circle / N_QUERIES );
+		out( "Average hits per query: " + n_circle / N_QUERIES );
+		out( "" );
+		out( "Search by box:" );
+		out( "Overall time for " + N_QUERIES + " queries [ms]: " + time_box );
+		out( "Average time per query [ms]: " + (double)time_box / N_QUERIES );
+		out( "Average hits per query: " + n_box / N_QUERIES );
 	}
 	
 	@After
@@ -78,6 +113,10 @@ public class MongoDBMassTest {
 		repo.deleteAll();
 	}
 
+	private static double rnd() {
+		return 2 * Math.random() -1;
+	}
+	
 	private static void out(Object o) {
 		System.out.println(o);
 	}
