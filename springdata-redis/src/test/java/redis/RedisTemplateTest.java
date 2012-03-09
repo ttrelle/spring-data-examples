b154db2c4f109770c0bd0325207a5e026a3c40af
Tobias Trelle <tobias.trelle@codecentric.de>
2012-03-09 13:05:01 +0100

    Renamed test case

--- springdata-redis/src/test/java/redis/RedisTemplateTest.java
+++ springdata-redis/src/test/java/redis/RedisTemplateTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class RedisRepoTest {
+public class RedisTemplateTest {
 	
 	@Autowired UserRepository repo;
 	
