924d8a843b762b73de97f16acdcc9cd8eb557af9
Tobias Trelle <tobias.trelle@codecentric.de>
2013-01-26 15:57:02 +0100

    separate db, default collection name

--- springdata-mongodb/src/main/java/mongodb/Order.java
+++ springdata-mongodb/src/main/java/mongodb/Order.java
@@ -7,7 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "order.springdata")
 public class Order {
 
 	@Id private String id;
