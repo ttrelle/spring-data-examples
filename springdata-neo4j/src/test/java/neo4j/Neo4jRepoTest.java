7f9d3e63f6fa34397f52ff15acee12820f779b4e
Tobias Trelle <tobias.trelle@codecentric.de>
2013-06-07 08:58:41 +0200

    Use current versions of Spring FW & Neo4j API

--- springdata-neo4j/src/test/java/neo4j/Neo4jRepoTest.java
+++ springdata-neo4j/src/test/java/neo4j/Neo4jRepoTest.java
@@ -1,8 +1,8 @@
 package neo4j;
 
-import java.util.List;
+import static org.junit.Assert.*;
 
-import junit.framework.Assert;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -17,77 +17,83 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class Neo4jRepoTest {
 
 	private static final int USER_COUNT = 4;
-	
-	@Autowired UserRepository repo;
-	
+
+	@Autowired
+	UserRepository repo;
+
 	private long rootId;
-	
-	@Before public void setUp() {
+
+	@Before
+	public void setUp() {
 		User root = new User("root", "Superuser");
 		User[] user = new User[USER_COUNT];
-		
-		for ( int i = 0; i < user.length; i++ ) {
-			user[i]  = new User(String.format("user%02d", i), "User" + i);
+
+		for (int i = 0; i < user.length; i++) {
+			user[i] = new User(String.format("user%02d", i), "User" + i);
 		}
 
 		// build graph
-		for ( int i = 0; i < user.length; i++ ) {
+		for (int i = 0; i < user.length; i++) {
 			root.knows(user[i]);
-			for ( int j = i; j < user.length; j++ ) {
+			for (int j = i; j < user.length; j++) {
 				user[i].knows(user[j]);
 			}
 		}
 
 		// save nodes
-		for ( int i = 0; i < user.length; i++ ) {
+		for (int i = 0; i < user.length; i++) {
 			repo.save(user[i]);
 		}
-		repo.save( root );
+		repo.save(root);
 		rootId = root.getId();
 		out("Root id: " + rootId);
-		
+
 	}
-	
-	@Test public void shouldFindAll() {
+
+	@Test
+	public void shouldFindAll() {
 		// when
 		long n = repo.count();
-		
+
 		// then
-		Assert.assertEquals("User count mismatch", USER_COUNT + 1, n);
+		assertEquals("User count mismatch", USER_COUNT + 1, n);
 	}
-	
-	@Test public void shouldFindRootUserById() {
+
+	@Test
+	public void shouldFindRootUserById() {
 		// when
 		User root = repo.findOne(rootId);
-		
+
 		// then
-		Assert.assertNotNull( "Root user not found", root );
+		assertNotNull("Root user not found", root);
 	}
-	
-	@Test public void shouldFindRootUserByLogin() {
+
+	@Test
+	public void shouldFindRootUserByLogin() {
 		// when
 		User root = repo.findByLogin("root");
-		
+
 		// then
-		Assert.assertNotNull( "Root user not found", root );
+		assertNotNull("Root user not found", root);
 	}
-	
-	@Test public void shouldFindFriendsOfRoot() {
+
+	@Test
+	public void shouldFindFriendsOfRoot() {
 		// when
 		List<User> users = repo.findFriendsOfRoot();
-		
+
 		// then
-		Assert.assertNotNull("result is <null>", users);
-		Assert.assertEquals( "mismatch @ friend count", USER_COUNT, users.size() );
+		assertNotNull("result is <null>", users);
+		assertEquals("mismatch @ friend count", USER_COUNT, users.size());
 	}
-	
+
 	@After
 	public void tearDown() {
 		repo.deleteAll();
 	}
-	
+
 	private static void out(Object o) {
 		System.out.println(o);
 	}
-	
+
 }
