1db992afcf4d12ba7a8e36b66543c40455bb5b23
tobias <tobias@10.10.60.97>
2017-04-21 10:38:54 +0200

    added submodule with skeleton for MongoDB REST repos

---
+++ spring-boot-mongo/src/main/java/de/codecentric/mongoboot/SpringBootMongoApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.mongoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoApplication.class, args);
+	}
+}
