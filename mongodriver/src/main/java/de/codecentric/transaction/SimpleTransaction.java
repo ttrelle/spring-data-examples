f5c55daa203081c5fde3b7d22e4fee553b91f5d6
Tobias Trelle <tobias.trelle@codecentric.de>
2018-08-31 12:13:00 +0200

    example for multi-document transaction

--- mongodriver/src/main/java/de/codecentric/transaction/SimpleTransaction.java
+++ mongodriver/src/main/java/de/codecentric/transaction/SimpleTransaction.java
@@ -2,20 +2,14 @@ package de.codecentric.transaction;
 
 import java.io.UnsupportedEncodingException;
 import java.net.UnknownHostException;
-import java.util.Date;
 
-import org.bson.BSON;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
-import com.mongodb.client.FindIterable;
+import com.mongodb.client.ClientSession;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
 
 /**
  * Simple transaction example.
@@ -24,40 +18,49 @@ public class SimpleTransaction {
 
 	/**
 	 * CLI call.
-	 * @param argv command line arguments
-	 * @throws MongoException 
-	 * @throws UnknownHostException 
-	 * @throws UnsupportedEncodingException 
+	 * 
+	 * @param argv
+	 *            command line arguments
+	 * @throws MongoException
+	 * @throws UnknownHostException
+	 * @throws UnsupportedEncodingException
 	 */
 	public static void main(String[] argv) throws UnknownHostException, MongoException, UnsupportedEncodingException {
-		MongoClient mongo = null;
-		
+		MongoClient client = null;
+
 		try {
 			// Default: localhost:27017
-			mongo = new MongoClient();
-			
+			client = new MongoClient();
+
 			// Sharding: mongos server
 			// mongo = new MongoClient("mongos-1", 4711);
-			
-			// Replica set
-//			mongo = new MongoClient(
-//					new MongoClientURI("mongodb://localhost:27001,localhost:27002,localhost:27003/replicaSet=demo-dev")
-//					);
-			
-			// use database "test"
-			MongoDatabase db = mongo.getDatabase("test");
 
-			// TODO implement example for multi-document transaction
-			
-			} finally {
-				if (mongo != null) {
-					mongo.close();
-				}
+			// Replica set
+			// client = new MongoClient(
+			// new
+			// MongoClientURI("mongodb://localhost:27001,localhost:27002,localhost:27003/replicaSet=demo-dev")
+			// );
+
+			// use database "test"
+			MongoDatabase db = client.getDatabase("test");
+			MongoCollection<Document> collection = db.getCollection("foo");
+
+			try (ClientSession clientSession = client.startSession()) {
+				clientSession.startTransaction();
+				collection.insertOne(clientSession, new Document("i", 1));
+				collection.insertOne(clientSession, new Document("i", 2));
+				clientSession.commitTransaction();
 			}
+
+		} finally {
+			if (client != null) {
+				client.close();
+			}
+		}
 	}
-	
+
 	private static final void println(Object o) {
 		System.out.println(o);
 	}
-	
+
 }
